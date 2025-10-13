3d712ec329728523199e5f1bb8abbc6e70f71cf0
Carlos Dubon <carlosminerodubon@gmail.com>
2025-10-13 15:32:08 -0600

    setup - adding  base libs and setting

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-red-500">Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
